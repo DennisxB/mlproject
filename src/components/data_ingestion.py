e22b7fa43f77e0cb1dcbb0d4465368a4194421fd
DennisxB <dkb7826@gmail.com>
2023-09-21 04:08:10 +0000

    testing model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_train import ModelTrainer
+from src.components.model_train import ModelTrainerConfig
 
 @dataclass # directly defining class variables
 class DataIngestionConfig:
@@ -37,7 +39,7 @@ class DataIngestion:
             logging.info('Read the dataset as dataframe')
 
             # Create the Artifacts directory
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             # Save the raw(full) data to the required path
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
@@ -61,5 +63,9 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    data_transformation = DataTransformation()
+    #data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr=train_arr, test_arr=test_arr))

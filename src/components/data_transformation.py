910dfc1b688f1ff1c5003947ffc37bac0eb2a1c1
DennisxB <dkb7826@gmail.com>
2023-09-24 03:40:07 +0000

    fixed errors

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -19,7 +19,7 @@ class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
-    def __init__(self) -> None:
+    def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
     # function responsible for data transformation
@@ -40,17 +40,17 @@ class DataTransformation:
             # Create numerical pipeline 
             num_pipeline = Pipeline(
                 steps=[
-                ("simple_impute", SimpleImputer(strategy="median")),
-                ("standard_scaler", StandardScaler())
+                    ("simple_impute", SimpleImputer(strategy="median")),
+                    ("standard_scaler", StandardScaler())
                 ]
             )
 
             # Create numerical pipeline 
             cat_pipeline = Pipeline(
                 steps=[
-                ("simple_impute", SimpleImputer(strategy="most frequent")),
-                ("one_hot_encoding", OneHotEncoder()),
-                ("standard_scaler", StandardScaler())
+                    ("simple_impute", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("standard_scaler", StandardScaler(with_mean=False))
                 ]
             )
 
@@ -58,18 +58,18 @@ class DataTransformation:
             logging.info(f"Categorical features: {categorical_features}")
             logging.info(f"Numerical features: {numerical_features}")
             
-            # Create the preprocesser pipeline
-            preprocesser = ColumnTransformer(
+            # Create the preprocessor pipeline
+            preprocessor = ColumnTransformer(
                 [
                     ("num_pipeline", num_pipeline, numerical_features),
                     ("cat_pipeline", cat_pipeline, categorical_features),
                 ]
             )
             
-            # Logginh the preprocessor pipeline process
+            # Logging the preprocessor pipeline process
             logging.info(f"Preprocessor pipeline is created.")
 
-            return preprocesser
+            return preprocessor
 
         
         except Exception as e:
@@ -79,35 +79,37 @@ class DataTransformation:
         try: 
             # Setup train and test DataFrame
             train_df = pd.read_csv(train_path)
-            test_df = pd.read(test_path)
+            test_df = pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
-            logging.info("Obtaining preprocessing object")
 
             # Instantiate the preprocessor object
-            preprocessor_obj = self.get_data_transformer_object()
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj = self.get_data_transformer_object()
 
             # Setup target feature name            
             target_feature_name = "math_score"
 
             # Split the independent and dependent variables for the train dataframe
-            input_feature_train_df = train_df.drop(columns=[target_feature_name], axis=1),
+            input_feature_train_df = train_df.drop(columns=[target_feature_name], axis=1)
             target_feature_train_df = train_df[target_feature_name]
             
             # Split the independent and dependent values for the test dataframe
-            input_feature_test_df = test_df.drop(columns=[target_feature_name], axis=1),
+            input_feature_test_df = test_df.drop(columns=[target_feature_name], axis=1)
             target_feature_test_df = test_df[target_feature_name]
 
             logging.info("Applying the preprocessing object on training and testing dataframe.")
 
             # Applying the preprocessor to train and validation data
-            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
+            # Create a 2D train tensor or matrix
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
 
+            # Create a 2D test tensor or matrix
             test_arr = np.c_[
                 input_feature_test_arr, np.array(target_feature_test_df)
             ]
@@ -117,14 +119,15 @@ class DataTransformation:
             # Save the pickle object
             save_object(
 
-                file_path = self.data_transformation_config.preprocessor_obj_file_path,
-                obj = preprocessor_obj
+                filepath = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
             )
 
+            # Return the train and test array, and the path containing the preprocessor 
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
         
         except Exception as e:

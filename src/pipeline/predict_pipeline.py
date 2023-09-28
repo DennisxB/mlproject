c4c534009a396a475a104ba8cc2e68cb2e436942
DennisxB <dkb7826@gmail.com>
2023-09-28 21:46:56 +0000

    fix inference errors

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -24,24 +24,14 @@ class PredictionPipeline:
         return prediction
         
 
-class CustomData(BaseModel):
-    def __init__(self, 
-            gender: str,
-            race_ethnicity: str,
-            parental_level_of_education: str,
-            lunch: str,
-            test_preparation_course: str,
-            reading_score: int,
-            writing_score: int):
-        
-        # Define the data attributes
-        self.gender = gender
-        self.race_ethnicity = race_ethnicity
-        self.parental_level_of_education = parental_level_of_education
-        self.lunch = lunch
-        self.test_preparation_course = test_preparation_course
-        self.reading_score = reading_score
-        self.writing_score = writing_score
+class CustomData(BaseModel): 
+    gender: str
+    race_ethnicity: str
+    parental_level_of_education: str
+    lunch: str
+    test_preparation_course: str
+    reading_score: int
+    writing_score: int
 
     def get_data_as_frame(self) -> pd.DataFrame:
         
@@ -52,10 +42,8 @@ class CustomData(BaseModel):
            "parental_level_of_education": [self.parental_level_of_education],
             "lunch": [self.lunch],
             "test_preparation_course": [self.test_preparation_course],
-            "math_score": [self.math_score],
             "reading_score": [self.reading_score],
             "writing_score": [self.writing_score]
         }
 
-        return pd.DataFrame(custom_data_input_dict)
-    
+        return pd.DataFrame(custom_data_input_dict)    
